59ff121adf9abf3effb9d79c5445189068e5c658
CodeshakeRus <codeshake-rus@yandex.ru>
2024-05-22 22:22:44 +0300

    finished refactoring checkboxField

--- src/shared/ui/checkbox-field/checkbox-field.tsx
+++ src/shared/ui/checkbox-field/checkbox-field.tsx
@@ -1,32 +1,31 @@
 import React from 'react';
 import './checkbox-field-style.css';
-import type { ChangeEvent } from 'react';
 import type { ICheckboxFieldProps } from './interafaces';
 
-const CheckboxField: React.FC<ICheckboxFieldProps> = ({ label, name, error, isRequired, titleHover, onChange, description, nameIcon, alt }) => {
-	function inputChange(event: ChangeEvent<HTMLInputElement>): void {
+const CheckboxField: React.FC<ICheckboxFieldProps> = ({ label, name, error, isRequired, titleHover, onChange, description, nameIcon, alt, value }) => {
+	function checkboxClick(): void {
 		if (onChange !== undefined) {
-			onChange({ key: name, value: event.target.checked });
+			onChange({ key: name, value: !value });
 		}
 	};
 
-	function handlerClickChecker() {
-
-	};
+	const classesChecker: string = 'checkbox-field__checker' + (value ? ' active' : '');
 
 	return (
 		<div className='checkbox-field checkbox-field__wrapper'>
 			<div className='checkbox-field__container'>
 				<p className='checkbox-field__description'>{description}</p>
 				<div title={titleHover} className='checkbox-field__content'>
-					<div onClick={handlerClickChecker} className='checkbox-field__input-wrapper'>
-						<input className='checkbox-field__input' onChange={inputChange} type='checkbox' name={name} id={name} />
-						<div className='checkbox-field__checker'>
-							<img className='checkbox-field__icon-checker' src={`/assets/icons/${nameIcon}`} alt={alt} />
+					<div onClick={checkboxClick} className='checkbox-field__input-wrapper'>
+						<input className='checkbox-field__input' type='checkbox' name={name} id={name} checked={value} value='' onChange={checkboxClick} />
+						<div className={classesChecker}>
+							{value &&
+								<img className='checkbox-field__icon-checker' src={`/assets/icons/${nameIcon}`} alt={alt} />
+							}
 						</div>
 					</div>
 					<div className='checkbox-field__label-wrapper'>
-						<label className='checkbox-field__label' onClick={handlerClickChecker} htmlFor={name}>{label}</label>
+						<label className='checkbox-field__label' onClick={checkboxClick}>{label}</label>
 						{isRequired &&
 							<span className='checkbox-field__required-flag'>*</span>
 						}
